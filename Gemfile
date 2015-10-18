6d32423df8f1c0687953e25222482adf01edda45
Carter Parks <carter@carterparks.com>
2015-10-18 11:06:24 -0600

    added callsign lookup, case insensitivity, and help

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'n0nbh', :github => 'carter/n0nbh'
+gem 'qrz-callbook'
 gem 'cinch'
 gem 'cinch-identify'
+
--- irc_bot.rb
+++ irc_bot.rb
@@ -1,9 +1,14 @@
 require 'cinch'
+
 gem 'n0nbh'
 require 'n0nbh'
-require "cinch/plugins/identify"
 
+gem 'qrz-callbook'
+require 'qrz-callbook'
+
+require "cinch/plugins/identify"
 require 'cinch/message'
+
 
 module Cinch
   class Message 
@@ -25,7 +30,7 @@ end
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.geekshed.net"
-    c.channels = ["#redditnet", "#fbom" ]
+    c.channels = ["#test123" ]
     c.nick = "HamBone"
      # add all required options here
     c.plugins.plugins = [Cinch::Plugins::Identify] # optionally add more plugins
@@ -35,12 +40,16 @@ bot = Cinch::Bot.new do
       :type     => :nickserv,
     }
   end
+  
+  on :message, /^!help$/i do |m|
+    m.reply "#{m.user.nick}: !hf - band conditions, !geo - solar conditions, !callsign <CALL> - callsign info"
+  end
 
-  on :message, "!geo" do |m|
+  on :message, /^!geo$/i do |m|
     m.reply "Solar Terrestrial Data - Solar Flux: #{N0nbh['solarflux']} - A-Index: #{N0nbh['aindex']} - K-Index: #{N0nbh['kindex']} / #{N0nbh['kindexnt']} - X-Ray: #{N0nbh['xray']} - Sunspots: #{N0nbh['sunspots']} - Helium Line: #{N0nbh['heliumline']} - Proton Flux: #{N0nbh['protonflux']} - Electron Flux: #{N0nbh['electonflux']} - Aurora: #{N0nbh['aurora']} - Normalization: #{N0nbh['normalization']} - Magnetic Field: #{N0nbh['magneticfield']} - Solar Wind: #{N0nbh['solarwind']} - http://n0nbh.com"
   end
 
-  on :message, "!hf" do |m|
+  on :message, /^!hf$/i do |m|
     m.reply "HF Conditions"
     m.reply "Band     Day   Night"
     %w(80m-40m 30m-20m 17m-15m 12m-10m).each do |band|
@@ -49,6 +58,33 @@ bot = Cinch::Bot.new do
       m.reply "#{band}  #{day}  #{night}".gsub('Poor', 'AIDS')
     end
     m.reply "MUF: #{N0nbh['muf']}mhz - Noise: #{N0nbh['signalnoise']} - http://n0nbh.com" 
+  end
+
+  on :message, /^!callsign (.*)/i do |m, callsign|
+    begin
+      call = QRZCallbook.new({ 'callsign' => callsign, 'username' => 'NS7I', 'password' => 'changethis' }).get_listing
+
+      operator = [call['fname'], call['name']].compact.join(' ')
+      operator << " (#{call['class']})" if call['class'] && call['class'] != ''
+
+      if call['land'] == call['country']
+        location_a = []
+        location_a << call['addr2'] if call['addr2'] && call['addr2'] != ''
+        location_a << call['county'] + ' County' if call['county'] && call['county'] != ''
+        location_a << call['state'] if call['state'] && call['state'] != ''
+        location_a << call['country'] if call['country'] && call['country'] != ''
+        location = location_a.compact.join(', ')
+      else
+        location = call['land']
+      end
+
+      location << " (#{call['grid']})" if call['grid'] && call['grid'] != ''
+        
+      response = [call['call'], operator, location, call['bio']].compact.join(' - ')
+    rescue QRZCallbook::SessionError => e
+      response = e.message
+    end
+    m.reply "#{m.user.nick}: #{response}"
   end
 end
 
